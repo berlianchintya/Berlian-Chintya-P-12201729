8e4cfa3109264b46794d3c9552ab0dcaf939b298
berlianchintya <115628184+berlianchintya@users.noreply.github.com>
2022-10-13 20:56:46 +0700

    Add files via upload

---
+++ doa.php
@@ -0,0 +1,18 @@
+<?php
+$data=file_get_contents("https://doa-doa-api-ahmadramadhan.fly.dev/api");
+$doa=json_decode($data);
+
+$table = "<h1>Kumpulan Doa Sehari-hari</h1>";
+$table .= "<table border=1> ";
+for($i=0;$i<count($doa);$i++){
+	$id=$i+1;
+	$table .= "<ol>
+				<li style=list-style:none;>{$id}. {$doa[$i]->doa}</li>
+				<ul><li>{$doa[$i]->ayat}</li>
+					<li>{$doa[$i]->latin}</li>
+					<li>{$doa[$i]->artinya}</li></ul>
+				</ol>";
+}
+$table .= "</table>";
+echo $table;
+?>
